e018856e9b53cfc848e96947ef0589efc81f371f
raldin14 <raldin.hidalgo@outlook.com>
2019-04-17 14:15:00 -0400

    creando funciones

---
+++ codBase/js/app.js
@@ -0,0 +1,24 @@
+var btnOn = document.getElementById('on');
+var btnSign = document.getElementById('sign');
+var btnRaiz = document.getElementById('raiz');
+var btnDiv = document.getElementById('dividido');
+var btn7 = document.getElementById('7');
+var btn8 = document.getElementById('8');
+var btn9 = document.getElementById('9');
+var btnPor = document.getElementById('por');
+var btn4 = document.getElementById('4');
+var btn5 = document.getElementById('5');
+var btn6 = document.getElementById('6');
+var btnMenos = document.getElementById('menos');
+var btn1 = document.getElementById('1');
+var btn2 = document.getElementById('2');
+var btn3 = document.getElementById('3');
+var btn0 = document.getElementById('0');
+var btnPunto = document.getElementById('punto');
+var btnIgual = document.getElementById('igual');
+var btnMas = document.getElementById('mas');
+
+var calculadora = {
+   
+
+}
