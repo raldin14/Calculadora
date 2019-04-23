72b3e6fda2e0c22b660e63b85ac5b1e6ce27bf05
raldin14 <raldin.hidalgo@outlook.com>
2019-04-23 16:54:52 -0400

    Agregando funciones de las operaciones matematicas

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -18,12 +18,96 @@ var btnPunto = document.getElementById('punto');
 var btnIgual = document.getElementById('igual');
 var btnMas = document.getElementById('mas');*/
 var botones = document.querySelectorAll('.teclado img');
-for(var i = 0; i < botones.length; i++){
+var pre, pos, operator;
+var decimal = false;
+for(var i = 0; i < botones.length; i++){    
     botones[i].onclick = function(e){
         var display = document.querySelector('#display');
+        var btnVal = e.target.id;
+        if(e.target.id == "on"){
+            //document.querySelector('#on').style = "padding: 0.5%";
+            display.innerHTML = 0;
+            operator = "";
+            pre = "";
+            pos = "";
+            decimal = false;
+        }else if(e.target.id == "igual"){
+            //document.querySelector('#on').style = "padding: 0.5%";
+            pos = display.innerHTML;
+            if(operator == "mas"){
+                sumar(pre, pos);
+            }else if(operator == "menos"){
+                restar(pre, pos);
+            }else if(operator == "por"){
+                multiplicar(pre, pos);
+            }else if(operator == "dividido"){
+                dividir(pre, pos);
+            }
+            
+        }else if(e.target.id == "punto"){
+            if(!decimal){
+                display.innerHTML += ".";
+                decimal = true;
+            }
+        }else if(e.target.id == "sign"){            
+            if(display.innerHTML != 0){
+                display.innerHTML = "-" + display.innerHTML;
+                decimal = true;
+            }
+        }else {           
+            if(display.innerHTML == 0){
+                if(decimal){
+                    display.innerHTML += btnVal;
+                }else{
+                    display.innerHTML = btnVal;
+                }
+            }else if(display.innerHTML.length != 8){
+                switch(e.target.id){
+                    case "mas":
+                        operator = e.target.id;
+                        pre = display.innerHTML;
+                        display.innerHTML = "";
+                        btnVal = "";
+                        break;                    
+                    case "menos":
+                        operator = e.target.id;
+                        pre = display.innerHTML;
+                        display.innerHTML = "";
+                        btnVal = "";
+                        break;
+                    case "por":
+                        operator = e.target.id;
+                        pre = display.innerHTML;
+                        display.innerHTML = "";
+                        btnVal = "";
+                        break;
+                    case "dividido":
+                        operator = e.target.id;
+                        pre = display.innerHTML;
+                        display.innerHTML = "";
+                        btnVal = "";
+                        break;
+                    default:
+                        break;
+                }
+                display.innerHTML += btnVal;
+            }
+        }
         
-        if(e.target.id == 8){
-            display.innerHTML = 8;
+        function sumar(pre, pos){
+            display.innerHTML = parseInt(pre) + parseInt(pos);
+        }
+
+        function restar(pre, pos){
+            display.innerHTML = pre - pos;
+        }
+
+        function dividir(pre, pos){
+            display.innerHTML = pre / pos;
+        }
+
+        function multiplicar(pre, pos){
+            display.innerHTML = pre * pos;
         }
     }
 }
